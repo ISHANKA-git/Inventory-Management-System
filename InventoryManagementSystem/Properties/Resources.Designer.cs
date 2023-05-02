e7868636d3c61b889e1376795a95b4f51945598e
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-02 19:54:14 +0530

    Update Resources.Designer.cs

--- InventoryManagementSystem/Properties/Resources.Designer.cs
+++ InventoryManagementSystem/Properties/Resources.Designer.cs
@@ -93,6 +93,26 @@ namespace InventoryManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap product {
+            get {
+                object obj = ResourceManager.GetObject("product", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap product_h {
+            get {
+                object obj = ResourceManager.GetObject("product h", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap R {
             get {
                 object obj = ResourceManager.GetObject("R", resourceCulture);
