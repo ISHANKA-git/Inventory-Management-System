2b699ff2f9ade1f2d979062849b2a6d493c68999
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-02 23:19:14 +0530

    Update MainForm.Designer.cs

--- InventoryManagementSystem/MainForm.Designer.cs
+++ InventoryManagementSystem/MainForm.Designer.cs
@@ -29,14 +29,14 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panelMain = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.customerButton6 = new InventoryManagementSystem.CustomerButton();
             this.customerButton3 = new InventoryManagementSystem.CustomerButton();
             this.customerButton4 = new InventoryManagementSystem.CustomerButton();
@@ -54,7 +54,7 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
@@ -70,13 +70,74 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(890, 93);
+            this.panel1.Size = new System.Drawing.Size(992, 96);
             this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label6.Location = new System.Drawing.Point(878, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 15);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "ORDERS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label5.Location = new System.Drawing.Point(781, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 15);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "USERS";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label4.Location = new System.Drawing.Point(660, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "CATEGORIES";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(552, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "CUSTOMERS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(454, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PRODUCTS";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Snow;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Goldenrod;
             this.label3.Location = new System.Drawing.Point(8, 64);
@@ -90,84 +151,26 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.SlateGray;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 498);
+            this.panel2.Location = new System.Drawing.Point(0, 602);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(890, 15);
+            this.panel2.Size = new System.Drawing.Size(992, 15);
             this.panel2.TabIndex = 0;
             // 
             // panelMain
             // 
             this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelMain.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panelMain.Location = new System.Drawing.Point(0, 93);
+            this.panelMain.Location = new System.Drawing.Point(0, 96);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawing.Size(890, 405);
+            this.panelMain.Size = new System.Drawing.Size(992, 506);
             this.panelMain.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label1.Location = new System.Drawing.Point(377, 64);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "PRODUCTS";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label2.Location = new System.Drawing.Point(475, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 16);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "CUSTOMERS";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label4.Location = new System.Drawing.Point(583, 64);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 16);
-            this.label4.TabIndex = 15;
-            this.label4.Text = "CATEGORIES";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label5.Location = new System.Drawing.Point(704, 64);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 16);
-            this.label5.TabIndex = 16;
-            this.label5.Text = "USERS";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.Goldenrod;
-            this.label6.Location = new System.Drawing.Point(801, 64);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 16);
-            this.label6.TabIndex = 17;
-            this.label6.Text = "ORDERS";
             // 
             // customerButton6
             // 
             this.customerButton6.Image = global::InventoryManagementSystem.Properties.Resources.Untitled_design;
             this.customerButton6.ImageHover = global::InventoryManagementSystem.Properties.Resources._11111;
             this.customerButton6.ImageNormal = global::InventoryManagementSystem.Properties.Resources.Untitled_design;
-            this.customerButton6.Location = new System.Drawing.Point(819, 26);
+            this.customerButton6.Location = new System.Drawing.Point(896, 24);
             this.customerButton6.Name = "customerButton6";
             this.customerButton6.Size = new System.Drawing.Size(30, 30);
             this.customerButton6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -179,7 +182,7 @@
             this.customerButton3.Image = global::InventoryManagementSystem.Properties.Resources.users;
             this.customerButton3.ImageHover = global::InventoryManagementSystem.Properties.Resources._111;
             this.customerButton3.ImageNormal = global::InventoryManagementSystem.Properties.Resources.users;
-            this.customerButton3.Location = new System.Drawing.Point(717, 26);
+            this.customerButton3.Location = new System.Drawing.Point(794, 24);
             this.customerButton3.Name = "customerButton3";
             this.customerButton3.Size = new System.Drawing.Size(30, 30);
             this.customerButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -191,7 +194,7 @@
             this.customerButton4.Image = global::InventoryManagementSystem.Properties.Resources.category;
             this.customerButton4.ImageHover = global::InventoryManagementSystem.Properties.Resources.c_3;
             this.customerButton4.ImageNormal = global::InventoryManagementSystem.Properties.Resources.category;
-            this.customerButton4.Location = new System.Drawing.Point(616, 26);
+            this.customerButton4.Location = new System.Drawing.Point(693, 24);
             this.customerButton4.Name = "customerButton4";
             this.customerButton4.Size = new System.Drawing.Size(30, 30);
             this.customerButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -203,7 +206,7 @@
             this.customerButton2.Image = global::InventoryManagementSystem.Properties.Resources.customer;
             this.customerButton2.ImageHover = global::InventoryManagementSystem.Properties.Resources.c_2;
             this.customerButton2.ImageNormal = global::InventoryManagementSystem.Properties.Resources.customer;
-            this.customerButton2.Location = new System.Drawing.Point(507, 26);
+            this.customerButton2.Location = new System.Drawing.Point(584, 24);
             this.customerButton2.Name = "customerButton2";
             this.customerButton2.Size = new System.Drawing.Size(30, 30);
             this.customerButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -215,7 +218,7 @@
             this.customerButton1.Image = global::InventoryManagementSystem.Properties.Resources.product;
             this.customerButton1.ImageHover = global::InventoryManagementSystem.Properties.Resources.product_h;
             this.customerButton1.ImageNormal = global::InventoryManagementSystem.Properties.Resources.product;
-            this.customerButton1.Location = new System.Drawing.Point(404, 26);
+            this.customerButton1.Location = new System.Drawing.Point(481, 24);
             this.customerButton1.Name = "customerButton1";
             this.customerButton1.Size = new System.Drawing.Size(30, 30);
             this.customerButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -236,7 +239,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 513);
+            this.ClientSize = new System.Drawing.Size(992, 617);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
