da7d7757628e4421c5d29814b20426de066880a5
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-02 23:40:39 +0530

    Update UserModuleForm.cs

--- InventoryManagementSystem/UserModuleForm.cs
+++ InventoryManagementSystem/UserModuleForm.cs
@@ -16,5 +16,10 @@ namespace InventoryManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
