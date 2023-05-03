1ca87bb409b27c3c6808986c60f3b4c914bb9ab9
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-03 21:48:57 +0530

    Update UserForm.cs

--- InventoryManagementSystem/UserForm.cs
+++ InventoryManagementSystem/UserForm.cs
@@ -38,13 +38,41 @@ namespace InventoryManagementSystem
             dr.Close();
             con.Close();
         }
-    
 
-
-private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            string colName = dgvUser.Columns[e.ColumnIndex].Name;
+            if (colName == "Edit")
+            {
+                UserModuleForm userModule = new UserModuleForm();
+                userModule.txtUserName.Text = dgvUser.Rows[e.RowIndex].Cells[1].Value.ToString();
+                userModule.txtFullName.Text = dgvUser.Rows[e.RowIndex].Cells[2].Value.ToString();
+                userModule.txtPass.Text = dgvUser.Rows[e.RowIndex].Cells[3].Value.ToString();
+                userModule.txtPhone.Text = dgvUser.Rows[e.RowIndex].Cells[4].Value.ToString();
+
+                userModule.btnSave.Enabled = false;
+                userModule.btnUpdate.Enabled = true;
+                userModule.txtUserName.Enabled = false;
+                userModule.ShowDialog();
+
+            }
+            else if (colName == "Delete")
+            {
+                if (MessageBox.Show("Are you sure want to delete this user?", "Delete Record", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    con.Open();
+                    cm = new SqlCommand("DELETE FROM tbUser WHERE username LIKE '" + dgvUser.Rows[e.RowIndex].Cells[1].Value.ToString() + "'", con);
+                    cm.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Record has been successfully deleted!");
+                }
+            }
+            LoadUser();
 
         }
+
+
+
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
@@ -52,7 +80,15 @@ private void dataGridView1_CellContentClick(object sender, DataGridViewCellEvent
             userModule.btnSave.Enabled = true;
             userModule.btnUpdate.Enabled = false;
             userModule.ShowDialog();
+            LoadUser();
 
+
+        }
+
+    
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
 
         }
     }
