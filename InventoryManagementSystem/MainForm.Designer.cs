a4ae59838b336fabf22b25e337b739b86a97de7b
Zupuu <121217903+Zupuu@users.noreply.github.com>
2023-05-03 16:16:24 +0530

    Update MainForm.Designer.cs

--- InventoryManagementSystem/MainForm.Designer.cs
+++ InventoryManagementSystem/MainForm.Designer.cs
@@ -51,7 +51,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.textBox1.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.Location = new System.Drawing.Point(99, 63);
             this.textBox1.Name = "textBox1";
@@ -75,9 +75,9 @@
             this.label2.Font = new System.Drawing.Font("Goudy Stout", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(11, 38);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(132, 64);
+            this.label2.Size = new System.Drawing.Size(153, 64);
             this.label2.TabIndex = 1;
-            this.label2.Text = "M \r\n";
+            this.label2.Text = "MS";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label1
