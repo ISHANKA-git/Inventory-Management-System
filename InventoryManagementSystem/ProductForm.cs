c3295d2e57b283be06c3638fb0bee3ba0d0e1498
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-04 13:02:07 +0530

    Create ProductForm.cs

---
+++ InventoryManagementSystem/ProductForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class ProductForm : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ishan\Documents\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cm = new SqlCommand();
+        SqlDataReader dr;
+        public ProductForm()
+        {
+            InitializeComponent();
+            LoadProduct();
+        }
+
+        public void LoadProduct()
+        {
+            int i = 0;
+            dgvProduct.Rows.Clear();
+            cm = new SqlCommand("SELECT * FROM tbProduct", con);
+            con.Open();
+            dr = cm.ExecuteReader();
+            while (dr.Read())
+            {
+                i++;
+                dgvProduct.Rows.Add(i, dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString());
+            }
+
+            dr.Close();
+            con.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ProductModuleForm formModule = new ProductModuleForm();
+            formModule.btnSave.Enabled = true;
+            formModule.btnUpdate.Enabled = false;
+            formModule.ShowDialog();
+            LoadProduct();
+            
+        }
+    }
+}
