2766034a0760c0ba31841533eb463fb59960f94a
IZAaa <87255708+ISHANKA-git@users.noreply.github.com>
2023-05-04 15:42:59 +0530

    Create OrderModuleForm.cs

---
+++ InventoryManagementSystem/OrderModuleForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class OrderModuleForm : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ishan\Documents\dbIMS.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cm = new SqlCommand();
+        SqlDataReader dr;
+        public OrderModuleForm()
+        {
+            InitializeComponent();
+            LoadCustomer();
+            LoadProduct();
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            
+        }
+
+        public void LoadCustomer()
+        {
+            int i = 0;
+            dgvCustomer.Rows.Clear();
+            cm = new SqlCommand("SELECT cid, cname FROM tbCustomer WHERE CONCAT(cid, cname) LIKE '%"+txtSearchCust.Text+"%'", con);
+            con.Open();
+            dr = cm.ExecuteReader();
+            while (dr.Read())
+            {
+                i++;
+                dgvCustomer.Rows.Add(i, dr[0].ToString(), dr[1].ToString());
+            }
+
+            dr.Close();
+            con.Close();
+        }
+
+        public void LoadProduct()
+        {
+            int i = 0;
+            dgvProduct.Rows.Clear();
+            cm = new SqlCommand("SELECT * FROM tbProduct WHERE CONCAT(pid, pname, pprice, pdescription, pcategory) LIKE '%" + txtSearchProd.Text + "%'", con);
+            con.Open();
+            dr = cm.ExecuteReader();
+            while (dr.Read())
+            {
+                i++;
+                dgvProduct.Rows.Add(i, dr[0].ToString(), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString());
+            }
+
+            dr.Close();
+            con.Close();
+        }
+
+        private void OrderModuleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvCustomer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void txtSearchCust_TextChanged(object sender, EventArgs e)
+        {
+            LoadCustomer();
+        }
+
+        private void txtSearchProd_TextChanged(object sender, EventArgs e)
+        {
+            LoadProduct();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
